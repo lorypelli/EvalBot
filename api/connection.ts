4e52b5356942c619eecb589cd33360003b4eced9
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2023-07-16 11:42:45 +0200

    use authorization header

--- api/connection.ts
+++ api/connection.ts
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose';
-const url = `mongodb+srv://EvalBot:${process.env.PASSWORD}@evalbot.crs0qn4.mongodb.net/EvalBot?retryWrites=true&w=majority`;
 export default async (request: import('@vercel/node').VercelRequest, response: import('@vercel/node').VercelResponse) => {
     if (request.method === 'POST') {
-        mongoose.connect(url).then(() => {
+        mongoose.connect(request.headers.authorization!).then(() => {
             response.status(202).send('Connected to the database');
         }).catch(() => {
             response.status(403).send('An error occurred while connecting');
