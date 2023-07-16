3f2d10e85fec876a41ede5ec34e1ed02ba001fae
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2023-07-16 11:51:27 +0200

    use only correct password

--- api/connection.ts
+++ api/connection.ts
@@ -4,10 +4,15 @@ export default async (request: import('@vercel/node').VercelRequest, response: i
         return response.status(405).send('Method not allowed');
     }
     if (request.method === 'POST') {
-        mongoose.connect(request.headers.authorization!).then(() => {
-            response.status(202).send('Connected to the database');
-        }).catch(() => {
-            response.status(403).send('An error occurred while connecting');
-        });
+        if (request.headers.authorization! == process.env.PASSWORD) {
+            mongoose.connect(request.headers.authorization!).then(() => {
+                response.status(202).send('Connected to the database');
+            }).catch(() => {
+                response.status(403).send('An error occurred while connecting');
+            });
+        }
+        else {
+            response.status(406).send('Wrong password');
+        }
     }
 };
