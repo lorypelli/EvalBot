b43441c890ff3beb5b5d8f1ded63489988e12463
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2023-07-16 19:00:48 +0200

    added another header

--- api/server_count.ts
+++ api/server_count.ts
@@ -1,6 +1,6 @@
 import { Guild } from 'serverless_bots_addons';
 export default async (request: import('@vercel/node').VercelRequest, response: import('@vercel/node').VercelResponse) => {
-    response.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
+    response.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000, https://evalbotbeta.vercel.app');
     if (request.method !== 'GET') {
         return response.status(405).send('Method not allowed');
     }
