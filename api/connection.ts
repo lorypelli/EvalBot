e9e377cb6ee3b4141abc17e48493fd57f8b6dadc
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2023-07-16 12:19:32 +0200

    Use 200 instead of 202

--- api/connection.ts
+++ api/connection.ts
@@ -7,7 +7,7 @@ export default async (request: import('@vercel/node').VercelRequest, response: i
     if (request.method === 'POST') {
         if (request.headers.authorization! == process.env.PASSWORD) {
             mongoose.connect(`mongodb+srv://EvalBot:${request.headers.authorization!}@evalbot.crs0qn4.mongodb.net/EvalBot?retryWrites=true&w=majority`).then(() => {
-                response.status(202).send('Connected to the database');
+                response.status(200).send('Connected to the database');
             }).catch(() => {
                 response.status(403).send('An error occurred while connecting');
             });
