538bc4e9f48e5d21e5d9deb2dd66011346bb8cef
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2023-07-16 11:41:14 +0200

    send a response

--- api/connection.ts
+++ api/connection.ts
@@ -3,9 +3,9 @@ const url = `mongodb+srv://EvalBot:${process.env.PASSWORD}@evalbot.crs0qn4.mongo
 export default async (request: import('@vercel/node').VercelRequest, response: import('@vercel/node').VercelResponse) => {
     if (request.method === 'POST') {
         mongoose.connect(url).then(() => {
-            response.status(202);
+            response.status(202).send('Connected to the database');
         }).catch(() => {
-            response.status(403);
+            response.status(403).send('An error occurred while connecting');
         });
     }
 };
